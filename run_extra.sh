e580c7a9da4ff47620dd1749e1ddf8eadd486dc8
Musu Yuan <52522175+ddb-qiwang@users.noreply.github.com>
2025-03-21 04:10:33 +0800

    Update run_extra.sh

--- run_extra.sh
+++ run_extra.sh
@@ -15,11 +15,11 @@ python p2_superpixel_quality_control.py ${prefix} --m 0.7
 python p3_feature_extraction.py  ${prefix} --device=${device} --model_path './checkpoints/' --down_samp_step 1
 
 # get histology segmentations
-python p4_get_histology_segmentation.py ${prefix} --down_samp_step 1 --num_histology_clusters 25
-python p5_merge_over_clusters.py ${prefix}
+#python p4_get_histology_segmentation.py ${prefix} --down_samp_step 1 --num_histology_clusters 25
+#python p5_merge_over_clusters.py ${prefix}
 
 # ROI selection
-python p6_roi_selection.py ${prefix} --down_samp_step 1 --roi_size 2 2 
+#python p6_roi_selection.py ${prefix} --down_samp_step 1 --roi_size 2 2 
 
 # cell-level label broadcasting
 python p7_cell_label_broadcasting.py ${prefix} --roi_size 2 2 
