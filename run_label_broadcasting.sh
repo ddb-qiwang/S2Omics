6ed3a03c0e5f7a5ccb0a7318d1a4476f429747c2
Musu Yuan <52522175+ddb-qiwang@users.noreply.github.com>
2025-10-11 11:14:52 +0800

    Update run_label_broadcasting.sh

--- run_label_broadcasting.sh
+++ run_label_broadcasting.sh
@@ -8,6 +8,7 @@ device="cuda"  # "cuda" or "cpu"
 pixel_size=0.5  # desired pixel size for the whole analysis
 
 # extract the histology feature of whole-slide H&E image
+echo $pixel_size > ${WSI_datapath}pixel-size.txt
 python p1_histology_preprocess.py ${WSI_datapath}
 python p2_superpixel_quality_control.py ${WSI_datapath} --patch_size 16
 python p3_feature_extraction.py  ${WSI_datapath} --foundation_model 'uni' --ckpt_path './checkpoints/uni/' --device=${device} --down_samp_step 1
