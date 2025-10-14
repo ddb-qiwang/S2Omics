2284a072ec0a6e17620de667200674c7fad86728
Musu Yuan <52522175+ddb-qiwang@users.noreply.github.com>
2025-10-14 19:03:48 +0800

    Delete run_label_broadcasting_demo.sh

--- run_label_broadcasting_demo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-WSI_datapath="./demo/colorectal_cancer_p1/"
-SO_datapath="./demo/colorectal_cancer_p1/"
-
-# run pipeline
-./run_label_broadcasting.sh $WSI_datapath $SO_datapath
